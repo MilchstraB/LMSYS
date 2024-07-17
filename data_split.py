631f9fa90762a0bd311fb014f0b0897e57fc07ad
MilchstraB <94272309+MilchstraB@users.noreply.github.com>
2024-07-17 18:40:47 +0800

    Update data_split.py

--- data_split.py
+++ data_split.py
@@ -3,6 +3,12 @@ from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("/h3cstore_nt/pc_embedding/mm3d/LMSYS/data/train.csv")
 
+n_dups = df.duplicated(subset=["prompt", "response_a", "response_b"], keep=False).sum()
+print(f"There exist {n_dups} duplicated rows.")
+df = df.drop_duplicates(
+    subset=["prompt", "response_a", "response_b"], keep="first", ignore_index=True
+)
+
 train_df, temp_df = train_test_split(df, test_size=0.2, random_state=42)
 val_df, test_df = train_test_split(temp_df, test_size=0.5, random_state=42)
 

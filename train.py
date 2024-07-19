ea579155eab00b82bf3f12a5d4f481e2acca5bf9
ForJadeForest <yingzhepeng@foxmail.com>
2024-07-20 00:19:05 +0800

    update

--- train.py
+++ train.py
@@ -45,15 +45,6 @@ class ModelArguments:
 @dataclass
 class DataArguments:
     train_data_path: str = field(
-        default=None, metadata={"help": "Path to the training data."}
-    )
-    val_data_path: str = field(
-        default=None, metadata={"help": "Path to the validation data."}
-    )
-    test_data_path: str = field(
-        default=None, metadata={"help": "Path to the test data."}
-    )
-    train_data_path: str = field(
         default="data/split/train.csv", metadata={"help": "Path to the training data."}
     )
     val_data_path: str = field(
@@ -65,22 +56,29 @@ class DataArguments:
 
 
 @dataclass
-class TrainingArguments(TrainingArguments):
-    cache_dir: Optional[str] = field(default=None)
-    optim: str = field(default="adamw_torch")
-    max_length: int = field(
-        default=2048,
-        metadata={
-            "help": "Maximum sequence length. Sequences will be right padded (and possibly truncated)."
-        },
-    )
-    lora_enable: bool = False
-    lora_r: int = 16
-    lora_alpha: int = 32
-    lora_dropout: float = 0.05
+class TrainingArguments(transformers.TrainingArguments):
+    lora_enable: bool = field(default=True)
+    lora_r: int = field(default=16)
+    lora_alpha: int = field(default=32)
+    lora_dropout: float = field(default=0.05)
     lora_bias: str = "none"
-    lora_target: Union[str, List] = "all-linear"
-    gradient_checkpointing_kwargs: Dict = {"use_reentrant": True}
+    lora_target = field(default="all-linear")
+    layers_to_transform: Optional[Union[list[int], int]] = field(default=None)
+    use_dora: bool = field(default=False)
+
+    gradient_checkpointing: bool = field(default=True)
+    eval_steps = field(default=0.2)
+    eval_strategy = field(default="steps")
+    eval_on_start = field(default=True)
+    bf16_full_eval = field(default=True)
+    output_dir = field(default="gemma_beseline_debug")
+    group_by_length = field(default=False)
+    debug_fast_test = field(default=False)
+
+    label_smoothing_factor = field(default=0.0)
+    warmup_ratio = field(default=0.05)
+    logging_steps = field(default=0.005)
+    report_to = field(default="wandb")
 
 
 def compute_metrics(eval_preds: EvalPrediction) -> dict:
@@ -97,18 +95,8 @@ def train():
         (ModelArguments, DataArguments, TrainingArguments)
     )
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
-    try:
-        training_args.lora_target = eval(training_args.lora_target)
-    except:
-        training_args.lora_target = training_args.lora_target
 
     # prepare tokenizer and model
-    tokenizer = AutoTokenizer.from_pretrained(
-        model_args.model_name_or_path, padding_side="right", use_fast=True
-    )
-    model = AutoModelForSequenceClassification.from_pretrained(
-        model_args.model_name_or_path, num_labels=3, torch_dtype=torch.bfloat16
-    )
     tokenizer = AutoTokenizer.from_pretrained(
         model_args.model_name_or_path,
         padding_side="right",
@@ -139,15 +127,26 @@ def train():
     val_dataset = Dataset.from_csv(data_args.val_data_path)
     test_dataset = Dataset.from_csv(data_args.test_data_path)
 
-    encode = CustomTokenizer(tokenizer, max_length=training_args.max_length)
+    if training_args.debug_fast_test:
+        train_dataset = train_dataset.select(range(5))
+        val_dataset = val_dataset.select(range(5))
+        test_dataset = test_dataset.select(range(20))
+    preprocess = CustomTokenizer(
+        tokenizer,
+        max_length=model_args.model_max_length,
+        prompt_template=model_args.prompt_template,
+        a_template=model_args.a_template,
+        b_template=model_args.b_template,
+        instruction=model_args.instruction,
+    )
     train_dataset = train_dataset.map(
-        encode, batched=True, remove_columns=train_dataset.column_names
+        preprocess, batched=True, remove_columns=train_dataset.column_names
     )
     val_dataset = val_dataset.map(
-        encode, batched=True, remove_columns=val_dataset.column_names
+        preprocess, batched=True, remove_columns=val_dataset.column_names
     )
     test_dataset = test_dataset.map(
-        encode, batched=True, remove_columns=test_dataset.column_names
+        preprocess, batched=True, remove_columns=test_dataset.column_names
     )
 
     trainer = Trainer(
@@ -155,7 +154,7 @@ def train():
         model=model,
         tokenizer=tokenizer,
         train_dataset=train_dataset,
-        eval_dataset={"val_ds": val_dataset},
+        eval_dataset=val_dataset,
         compute_metrics=compute_metrics,
         data_collator=DataCollatorWithPadding(tokenizer=tokenizer),
     )

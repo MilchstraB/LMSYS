d893f51aa81c679bf1a0013ee5215e64513e383e
ForJadeForest <yingzhepeng@foxmail.com>
2024-07-20 00:00:38 +0800

    add prompt template

--- train.py
+++ train.py
@@ -1,28 +1,31 @@
-import os
 import json
-import transformers
-from transformers import (
-    TrainingArguments,
-    PreTrainedTokenizerBase,
-    AutoTokenizer,
-    AutoModelForSequenceClassification,
-    EvalPrediction,
-    Trainer,
-    DataCollatorWithPadding,
-)
-from peft import LoraConfig, get_peft_model, TaskType
-from datasets import Dataset
-from typing import Optional, List, Union, Dict
+import os
 from dataclasses import dataclass, field
-from sklearn.metrics import log_loss, accuracy_score
+from typing import Dict, List, Optional, Union
 
 import torch
+import transformers
+from datasets import Dataset
+from peft import LoraConfig, TaskType, get_peft_model
+from sklearn.metrics import accuracy_score, log_loss
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    DataCollatorWithPadding,
+    EvalPrediction,
+    Trainer,
+    TrainingArguments,
+)
+
+from utils import CustomTokenizer
+
+os.environ["WANDB_PROJECT"] = "LMSYS_Text_ClS"
 
 
 @dataclass
 class ModelArguments:
     model_name_or_path: Optional[str] = field(
-        default="/home/share/pyz/model_weight/gemma-2-9b-it"
+        default="/data/share/pyz/model_weight/gemma-2-9b-it"
     )
     model_max_length: int = field(
         default=2048,
@@ -30,18 +33,24 @@ class ModelArguments:
             "help": "Maximum sequence length. Sequences will be right padded (and possibly truncated)."
         },
     )
+    instruction: str = field(
+        default="Now I will give you a prompt and two responses. You should choose the better response.\n"
+    )
+    prompt_template: dict = field(default="Prompt: <\P>")
+    a_template: str = field(default="Response of A: <\A>")
+    b_template: str = field(default="Response of B: <\B>")
 
 
 @dataclass
 class DataArguments:
     train_data_path: str = field(
-        default=None, metadata={"help": "Path to the training data."}
+        default="data/split/train.csv", metadata={"help": "Path to the training data."}
     )
     val_data_path: str = field(
-        default=None, metadata={"help": "Path to the validation data."}
+        default="data/split/val.csv", metadata={"help": "Path to the validation data."}
     )
     test_data_path: str = field(
-        default=None, metadata={"help": "Path to the test data."}
+        default="data/split/test.csv", metadata={"help": "Path to the test data."}
     )
 
 
@@ -49,55 +58,29 @@ class DataArguments:
 class TrainingArguments(transformers.TrainingArguments):
     cache_dir: Optional[str] = field(default=None)
     optim: str = field(default="adamw_torch")
-    lora_enable: bool = True
-    lora_r: int = 16
-    lora_alpha: int = 32
-    lora_dropout: float = 0.05
+
+    lora_enable: bool = field(default=True)
+    lora_r: int = field(default=16)
+    lora_alpha: int = field(default=32)
+    lora_dropout: float = field(default=0.05)
     lora_bias: str = "none"
-    gradient_checkpointing: bool = True
-    lora_target = "all-linear"
-    eval_steps = 0.2
-    eval_strategy = "steps"
-    eval_on_start = True
-    bf16_full_eval = True
-    output_dir = "gemma_beseline_debug"
-    resume_from_checkpoint = None
-    group_by_length = True
-    debug_fast_test = False
-    label_smoothing_factor = 0.0
-    warmup_ratio = 0.05
-    logging_steps = 0.005
+    layers_to_transform: Optional[Union[list[int], int]] = field(default=None)
+    use_dora: bool = field(default=False)
 
+    gradient_checkpointing: bool = field(default=True)
+    lora_target = field(default="True")
+    eval_steps = field(default=0.2)
+    eval_strategy = field(default="steps")
+    eval_on_start = field(default=True)
+    bf16_full_eval = field(default=True)
+    output_dir = field(default="gemma_beseline_debug")
+    group_by_length = field(default=False)
+    debug_fast_test = field(default=False)
 
-class CustomTokenizer:
-    def __init__(self, tokenizer: PreTrainedTokenizerBase, max_length: int) -> None:
-        self.tokenizer = tokenizer
-        self.max_length = max_length
-
-    def __call__(self, batch: dict) -> dict:
-        prompt = ["<prompt>: " + self.process_text(t) for t in batch["prompt"]]
-        response_a = [
-            "\n\n<response_a>: " + self.process_text(t) for t in batch["response_a"]
-        ]
-        response_b = [
-            "\n\n<response_b>: " + self.process_text(t) for t in batch["response_b"]
-        ]
-        texts = [p + r_a + r_b for p, r_a, r_b in zip(prompt, response_a, response_b)]
-        tokenized = self.tokenizer(texts, max_length=self.max_length, truncation=True)
-        labels = []
-        for a_win, b_win in zip(batch["winner_model_a"], batch["winner_model_b"]):
-            if a_win:
-                label = 0
-            elif b_win:
-                label = 1
-            else:
-                label = 2
-            labels.append(label)
-        return {**tokenized, "labels": labels}
-
-    @staticmethod
-    def process_text(text: str) -> str:
-        return " ".join(eval(text, {"null": ""}))
+    label_smoothing_factor = field(default=0.0)
+    warmup_ratio = field(default=0.05)
+    logging_steps = field(default=0.005)
+    report_to = field(default="wandb")
 
 
 def compute_metrics(eval_preds: EvalPrediction) -> dict:
@@ -120,7 +103,7 @@ def train():
         model_args.model_name_or_path, padding_side="right", use_fast=True
     )
     model = AutoModelForSequenceClassification.from_pretrained(
-        model_args.model_name_or_path, num_labels=3, torch_dtype=torch.bfloat16
+        model_args.model_name_or_path, num_labels=3
     )
     model.enable_input_require_grads()
     model.config.use_cache = False
@@ -132,7 +115,9 @@ def train():
             target_modules=training_args.lora_target,
             lora_dropout=training_args.lora_dropout,
             bias=training_args.lora_bias,
+            layers_to_transform=training_args.layers_to_transform,
             task_type=TaskType.SEQ_CLS,
+            use_dora=training_args.use_dora,
         )
         model = get_peft_model(model, lora_config)
 
@@ -145,15 +130,22 @@ def train():
         train_dataset = train_dataset.select(range(5))
         val_dataset = val_dataset.select(range(5))
         test_dataset = test_dataset.select(range(20))
-    encode = CustomTokenizer(tokenizer, max_length=model_args.model_max_length)
+    preprocess = CustomTokenizer(
+        tokenizer,
+        max_length=model_args.model_max_length,
+        prompt_template=model_args.prompt_template,
+        a_template=model_args.a_template,
+        b_template=model_args.b_template,
+        instruction=model_args.instruction,
+    )
     train_dataset = train_dataset.map(
-        encode, batched=True, remove_columns=train_dataset.column_names
+        preprocess, batched=True, remove_columns=train_dataset.column_names
     )
     val_dataset = val_dataset.map(
-        encode, batched=True, remove_columns=val_dataset.column_names
+        preprocess, batched=True, remove_columns=val_dataset.column_names
     )
     test_dataset = test_dataset.map(
-        encode, batched=True, remove_columns=test_dataset.column_names
+        preprocess, batched=True, remove_columns=test_dataset.column_names
     )
 
     trainer = Trainer(
@@ -161,7 +153,7 @@ def train():
         model=model,
         tokenizer=tokenizer,
         train_dataset=train_dataset,
-        eval_dataset={"val_ds": val_dataset},
+        eval_dataset=val_dataset,
         compute_metrics=compute_metrics,
         data_collator=DataCollatorWithPadding(tokenizer=tokenizer),
     )

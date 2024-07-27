f562fdb7e4094c959e9ec0f1d8b42f3cb2fc3ccf
MilchstraB <94272309+MilchstraB@users.noreply.github.com>
2024-07-27 12:41:37 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,19 @@
-from typing import Any
+import re
+import torch
 from transformers import PreTrainedTokenizerBase
+
+
+def is_only_whitespace(s):
+    return re.fullmatch(r'\s*', s) is not None
+
+
+def print_rank_0(message):
+    """If distributed is initialized, print only on rank 0."""
+    if torch.distributed.is_initialized():
+        if torch.distributed.get_rank() == 0:
+            print(message, flush=True)
+    else:
+        print(message, flush=True)
 
 
 class CustomTokenizer:
@@ -13,6 +27,7 @@ class CustomTokenizer:
         instruction: str,
         show_length: bool = False,
         use_chat_template: bool = False,
+        switch: bool = False,
     ) -> None:
         self.tokenizer = tokenizer
         self.max_length = max_length
@@ -22,6 +37,7 @@ class CustomTokenizer:
         self.instruction = instruction
         self.show_length = show_length
         self.use_chat_template = use_chat_template
+        self.switch = switch
 
     def __call__(self, batch: dict) -> dict:
         if self.show_length == False:
@@ -52,6 +68,10 @@ class CustomTokenizer:
             self.prompt_template.replace("<\P>", self.process_text(t))
             for t in batch["prompt"]
         ]
+
+        if self.switch:
+            response_a, response_b = response_b, response_a
+        
         texts = [
             self.instruction + "\n".join(sample)
             for sample in zip(prompt, response_a, response_b)
@@ -88,8 +108,57 @@ class CustomTokenizer:
             else:
                 label = 2
             labels.append(label)
+
+        if self.switch:
+            mapping = [1, 0, 2]
+            labels = [mapping[e] for e in labels]
         return {**tokenized_truncation, "labels": labels, "token_length": token_length}
 
     @staticmethod
     def process_text(text: str) -> str:
         return " ".join(eval(text, {"null": ""}))
+
+
+class ConvTokenizer:
+    """Concat the text with turn format: [prompt_1, response_a_1, response_b_1, prompt_2, response_a_2, response_b_2, ...]
+    """
+    def __init__(self, tokenizer: PreTrainedTokenizerBase, max_length: int) -> None:
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __call__(self, batch: dict) -> dict:
+        prompt = [self.process_text(t) for t in batch["prompt"]]
+        response_a = [self.process_text(t) for t in batch["response_a"]]
+        response_b = [self.process_text(t) for t in batch["response_b"]]
+
+        texts = []
+        for turns_p, turns_a, turns_b in zip(prompt, response_a, response_b):
+            conversation, round = (
+                "Which one of the chatbots below did a better job responding to the user request? Or were they tied?",
+                1,
+            )
+            for p, a, b in zip(turns_p, turns_a, turns_b):
+                if is_only_whitespace(p) or is_only_whitespace(a) or is_only_whitespace(b):
+                    continue
+                conversation += f"\n\n~~~~~~~~~~ ROUND {round} ~~~~~~~~~~"
+                conversation += f"\n\n<prompt>: {p}"
+                conversation += f"\n\n<response_a>: {a}"
+                conversation += f"\n\n<response_b>: {b}"
+                round += 1
+            texts.append(conversation)
+
+        tokenized = self.tokenizer(texts, max_length=self.max_length, truncation=True)
+        labels = []
+        for a_win, b_win in zip(batch["winner_model_a"], batch["winner_model_b"]):
+            if a_win:
+                label = 0
+            elif b_win:
+                label = 1
+            else:
+                label = 2
+            labels.append(label)
+        return {**tokenized, "labels": labels}
+
+    @staticmethod
+    def process_text(text: str) -> str:
+        return eval(text, {"null": ""})
